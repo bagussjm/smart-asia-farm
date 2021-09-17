e2c1d3d8b30c5f5fe573dedbb1283b7a6e34061c
bagussjm <bagusibag85@gmail.com>
2021-09-17 20:50:24 +0700

    add authenticated user unprocessed carts

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Keranjang::class,'id_user','id');
     }
+
+    public function cartsUnprocessed()
+    {
+        return $this->hasMany(Keranjang::class,'id_user','id')->unprocessed();
+    }
 }
