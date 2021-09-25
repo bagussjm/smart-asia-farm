6982b7bbf9da7bd5d46a0bfb22480146b063c396
bagussjm <bagusibag85@gmail.com>
2021-09-25 16:13:58 +0700

    update dashboard view
    - done bug qr code generator

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,17 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use SimpleSoftwareIO\QrCode\Generator;
 
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $qr = new Generator();
-        dd($qr->generate('kambing'));
         return view('backend.dashboard');
     }
 }
