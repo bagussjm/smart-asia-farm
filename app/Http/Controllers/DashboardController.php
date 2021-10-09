0b29c81fd4732bfce38aa8a49435d0d2086013fc
bagussjm <bagusibag85@gmail.com>
2021-10-09 13:34:41 +0700

    update png test

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Wahana;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class DashboardController extends Controller
@@ -13,10 +14,10 @@ class DashboardController extends Controller
     public function index()
     {
         try{
-            $img = QrCode::format('png')->generate('kambing');
-            dd(
-                $img
-            );
+            $fileName = 'kambing.svg';
+            $svg = QrCode::generate($fileName);
+            Storage::put('public/qr/png/'.$fileName,$svg);
+            return  Storage::url('/qr/png/'.$fileName);
         }catch (\Exception $exception){
             Log::error($exception->getMessage());
             dd(
