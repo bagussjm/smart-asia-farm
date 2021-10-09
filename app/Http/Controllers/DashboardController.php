c82ce052a7fe14def6d8346bcfa61c890a2631b6
bagussjm <bagusibag85@gmail.com>
2021-10-09 13:42:42 +0700

    done image png test

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,18 +13,18 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        try{
-            $fileName = 'mawar-hitam-x-marwah-riau.png';
-            $svg = QrCode::format('png')->generate($fileName);
-            Storage::put('public/qr/png/'.$fileName,$svg);
-            return  Storage::url('/qr/png/'.$fileName);
-        }catch (\Exception $exception){
-            Log::error($exception->getMessage());
-            dd(
-                $exception->getMessage()
-            );
-        }
+//        try{
+//            $fileName = 'mawar-hitam-x-marwah-riau.png';
+//            $svg = QrCode::format('png')->generate($fileName);
+//            Storage::put('public/qr/png/'.$fileName,$svg);
+//            return  Storage::url('/qr/png/'.$fileName);
+//        }catch (\Exception $exception){
+//            Log::error($exception->getMessage());
+//            dd(
+//                $exception->getMessage()
+//            );
+//        }
 
-//        return view('backend.dashboard');
+        return view('backend.dashboard');
     }
 }
