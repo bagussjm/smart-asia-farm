527d2b589b293332272fa3f92509c1643bde2fc3
bagussjm <bagusibag85@gmail.com>
2021-12-04 00:29:04 +0700

    done backend matching to API  Spec

--- app/Http/Controllers/Api/TicketApiController.php
+++ app/Http/Controllers/Api/TicketApiController.php
@@ -20,13 +20,49 @@ class TicketApiController extends ApiController
         $this->TicketRepository = $ticketRepository;
     }
 
-    public function show($ticket)
+    public function show($tiket)
     {
         try{
-            $ticket = $this->TicketRepository->find($ticket)->get();
+//            $ticket = $this->TicketRepository->find($ticket)->get();
+            $cartsInTicket = Tiket::with(['carts' => function($q){
+                $q->with('playground');
+            },'entranceTicket'])->findOrFail($tiket);
+            $ticket = collect([
+                'id' => $cartsInTicket->id,
+                'tanggal_masuk' => $cartsInTicket->tanggal_masuk,
+                'jam_masuk' => $cartsInTicket->jam_masuk,
+                'status' => $cartsInTicket->status,
+                'total_bayar' => $cartsInTicket->total_bayar,
+                'kode_qr' => $cartsInTicket->kode_qr,
+                'instruksi_pembayaran' => $cartsInTicket->instruksi_pembayaran,
+                'created_at' => $cartsInTicket->created_at,
+                'updated_at' => $cartsInTicket->updated_at,
+                'deleted_at' => $cartsInTicket->deleted_at
+            ]);
+
+            if ($cartsInTicket->entranceTicket->id !== null && $cartsInTicket->carts->count() > 0){
+                $ticket->put('tipe_tiket','A');
+            }elseif ($cartsInTicket->entranceTicket->id !== null && $cartsInTicket->carts->count() === 0){
+                $ticket->put('tipe_tiket','B');
+            }elseif ($cartsInTicket->entranceTicket->id === null && $cartsInTicket->carts->count() > 0){
+                $ticket->put('tipe_tiket','C');
+            }
+
+            $ridesInTicket = collect([]);
+            $cartsInTicket->carts->map(function ($value,$key) use ($ridesInTicket){
+                $ridesInTicket->push($value->playground);
+            });
+
+            $ticket->put('wahana',$ridesInTicket->all());
+
+            if ($cartsInTicket->entranceTicket->id !==null){
+                $ticket->put('tiket_masuk',$cartsInTicket->entranceTicket);
+            }else{
+                $ticket->put('tiket_masuk',null);
+            }
             return $this->successResponse(
-                new TiketResource($ticket),
-                'success, show ticket '.$ticket->id
+                $ticket->all(),
+                'success, show ticket '.$tiket
             );
         }catch (\Exception $exception){
             return $this->errorResponse(
